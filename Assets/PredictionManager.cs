b55fa8c66e526d765e3d2667fcd1607bc9c29394
rOwe <100542487+rOwe08@users.noreply.github.com>
2024-04-06 22:07:14 +0200

    Adding title panel

--- Assets/PredictionManager.cs
+++ Assets/PredictionManager.cs
@@ -24,6 +24,7 @@ public class PredictionManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Debug.Log(borders.transform.childCount);
     }
 
     // Update is called once per frame
